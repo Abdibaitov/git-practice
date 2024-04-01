0a52f2c1df5b15c37fe2587bc1ef0ea50c4ac304
Abdibaitov <91374399+Abdibaitov@users.noreply.github.com>
2024-04-01 14:20:40 +0300

    first commit

---
+++ src/Laptop.java
@@ -0,0 +1,10 @@
+public class Laptop {
+    String model;
+    int a;
+    int b;
+
+    public void sum(int a, int b){
+        System.out.println("Summa is:");
+        System.out.println("Sum:"+(a+b));
+    }
+}
